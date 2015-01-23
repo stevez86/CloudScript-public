48858b70493f586c58a233158e493fd326768c42
Steve Zimmerman <szman86@gmail.com>
2015-01-23 11:56:11 -0800

    working on views

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,20 @@ router.get('/', function(req, res, next) {
   res.send("Hello world!")
 });
 
+/* Rx page */
+/* GET rx page */
+router.get('/rx', function(req, res, next) {
+  res.sendfile("./public/views/rx.html")
+});
+
+/* POST rx page */
+router.post('/rx', function(req, res, next) {
+  var new_prescription = req.body
+  //get dleivery location - default to home?
+  //
+  var destination = {address: }
+  //get quote from postmates
+  return 'hi'
+});
+
 module.exports = router;
