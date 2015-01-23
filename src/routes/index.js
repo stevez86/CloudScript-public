7e838dec196e085a76494799d5c50a1ea26e68d9
Devon Estes <devon.c.estes@gmail.com>
2015-01-23 11:16:18 -0800

    testing framework setup and working

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var http = require('http')
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ src/test/chatAppSpec.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+var setupNocks = require('setup-nocks');
+
+describe('OperaLocal', function() {
+
+  var recorder = setupNocks.prototype.record('CloudScript', {nock_file_path: 'fixtures/CloudScript.js'});
+  before(recorder.before);
+
+  describe('GET "/"', function () {
+    it('is is a valid route', function (done) {
+      request("http://localhost:3000")
+        .get('/')
+        .end(function(err, res) {
+          expect(res.statusCode).to.eq(200)
+          done();
+      });
+    });
+  });
+
+});
