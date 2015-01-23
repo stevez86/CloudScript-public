3215cf72b58660b09bea40c4e6a69b4f82ada340
Devon Estes <devon.c.estes@gmail.com>
2015-01-22 20:15:41 -0800

    fixing error in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var database_connection = require('./database_config');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
