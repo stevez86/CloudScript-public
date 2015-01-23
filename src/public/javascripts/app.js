cc12e8b8ac29aa3d825fe0f2c482ec9f25cc6512
Devon Estes <devon.c.estes@gmail.com>
2015-01-23 12:32:20 -0800

    connected front end to api to pull messages

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -3,35 +3,10 @@
 
   app.controller('ChatController', ['$scope', '$http', function($scope, $http){
 
-    $scope.messages = [
-      {
-        content: "Hey doc, I'm feeling kind of weird.",
-        timestamp: "Today",
-        author: {
-          patient: true,
-          name: "John Smith",
-          avatar_url: "http://i1-news.softpedia-static.com/images/news2/Google-Chrome-18-Lands-in-the-Dev-Channel-Adds-User-Name-and-Avatar-Sync-2.png",
-          prescriptions: [
-            {
-            name: "Advil",
-            qty: 60,
-            ordered: false,
-            read: false
-            }
-          ],
-          home_address: "60 Oak st.",
-        }
-      },
-      {
-        content: "I'm sorry to hear that, John. Tell me more.",
-        timestamp: "Today",
-        author: {
-          doctor: true,
-          name: "Dr. John Doe",
-          avatar_url: "http://images.wisegeek.com/male-doctor.jpg"
-          }
-      }
-    ];
+    $http.get("/api/messages")
+      .success(function(data, status, headers, config) {
+        $scope.messages = data;
+      });
 
     this.sendText = function(text) {
       $scope.text = "";
