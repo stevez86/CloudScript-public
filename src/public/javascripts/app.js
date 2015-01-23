33986736c564af4ea503a530386d9f6849eca710
Devon Estes <devon.c.estes@gmail.com>
2015-01-23 12:27:38 -0800

    chat now functioning client side

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -3,7 +3,40 @@
 
   app.controller('ChatController', ['$scope', '$http', function($scope, $http){
 
-    $scope.messages = [{message: "I'm message one!"}, {message: "I'm message one!"}]
+    $scope.messages = [
+      {
+        content: "Hey doc, I'm feeling kind of weird.",
+        timestamp: "Today",
+        author: {
+          patient: true,
+          name: "John Smith",
+          avatar_url: "http://i1-news.softpedia-static.com/images/news2/Google-Chrome-18-Lands-in-the-Dev-Channel-Adds-User-Name-and-Avatar-Sync-2.png",
+          prescriptions: [
+            {
+            name: "Advil",
+            qty: 60,
+            ordered: false,
+            read: false
+            }
+          ],
+          home_address: "60 Oak st.",
+        }
+      },
+      {
+        content: "I'm sorry to hear that, John. Tell me more.",
+        timestamp: "Today",
+        author: {
+          doctor: true,
+          name: "Dr. John Doe",
+          avatar_url: "http://images.wisegeek.com/male-doctor.jpg"
+          }
+      }
+    ];
+
+    this.sendText = function(text) {
+      $scope.text = "";
+      $scope.messages.push({content: text})
+    }
 
   }]);
 
