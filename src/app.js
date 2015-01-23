cdb88485c4f91aa1c5f699b35295b181c7e48db7
Devon Estes <devon.c.estes@gmail.com>
2015-01-22 19:51:48 -0800

    trimmed file structure down to bare minimum so we can build up from here with minimum errors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var database_connection = require('./database_config');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
