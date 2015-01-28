77a59b4f1d87fe4f16930cd945219936a7592878
pfeffer629 <pfeffer629@gmail.com>
2015-01-27 21:37:39 -0800

    new modularized file structure

---
+++ src/public/components/patient/patientController.js
@@ -0,0 +1,21 @@
+app.controller('PatientController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+
+  var patientID = $routeParams.patientid; //set this to current patient id $scope.current_user.id
+
+  //patients info
+  $http.get('/api/patients/'+ patientID)
+    .success(function(data, status, headers, config) {
+      console.log(data);
+      $scope.patient = data;
+  })
+
+  var doctorID = $routeParams.doctorid;
+
+  if (doctorID) {
+
+    $http.get('/api/patients/'+ patientID + '/doctors/' + doctorID)
+      .success(function(data, status, headers, config) {
+        $scope.doctor = data;
+    })
+  }
+}]);
---
+++ src/public/shared/chat/chatDirective.js
@@ -0,0 +1,6 @@
+app.directive('chat', function(){
+  return {
+    restrict: 'E',
+    templateUrl: '../partials/chat.html',
+  };
+});
